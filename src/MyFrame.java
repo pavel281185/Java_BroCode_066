92ebf5376a9e0607cea0e54cab393d3adaa98ce8
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 21:16:56 +0200

    Initial commit

---
+++ src/MyFrame.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class MyFrame extends JFrame implements ActionListener {
+    JButton button;
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Select");
+        button.addActionListener(this);
+
+        this.add(button);
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            JFileChooser fileChooser = new JFileChooser();
+
+            fileChooser.setCurrentDirectory(new File("."));
+
+//            int response = fileChooser.showOpenDialog(null);
+            int response = fileChooser.showSaveDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
+        }
+    }
+}
